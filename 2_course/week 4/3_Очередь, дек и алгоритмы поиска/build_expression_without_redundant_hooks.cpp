c16648b5a529b949d34218191d33cfe8cf767aa6
Alex Petrenko <a.petrenko@rczifort.local>
2018-10-02 16:03:11 +0300

    week 4 done!

--- 2_course/week 4/3_Очередь, дек и алгоритмы поиска/build_expression_without_redundant_hooks.cpp
+++ 2_course/week 4/3_Очередь, дек и алгоритмы поиска/build_expression_without_redundant_hooks.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <deque>
+#include <stack>
 
 using namespace std;
 
@@ -13,28 +14,37 @@ int main()
     cin >> n;
 
     deque<string> formula;
+    stack<string> stack_opr;
     string symbol;
-    
+
     if (n < 1)
     {
         cout << x << endl;
     }
     else
     {
-        formula.push_back(to_string(x));            
+        formula.push_back(to_string(x));
         for (int i = 0; i < n; ++i)
         {
-            formula.push_front("(");
-            formula.push_back(")");
             cin >> symbol;
+            if (stack_opr.size() != 0)
+            {
+                if ((stack_opr.top() == "-") && (symbol == "*" || symbol == "/") ||
+                    (stack_opr.top() == "+") && (symbol == "*" || symbol == "/"))
+                {
+                    formula.push_front("(");
+                    formula.push_back(")");
+                }
+            }
+            stack_opr.push(symbol);
             cin >> x;
             formula.push_back(" " + symbol + " " + to_string(x));
         }
         for (auto it : formula)
         {
             cout << it;
-        }        
+        }
     }
-    
+
     return 0;
 }
