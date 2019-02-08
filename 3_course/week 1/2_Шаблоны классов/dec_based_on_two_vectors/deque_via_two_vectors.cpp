1d86af93c44fa0306985219ceae3ad6980409fd0
Alex Petrenko <a.petrenko@rczifort.local>
2019-02-08 20:57:27 +0300

    new

--- 3_course/week 1/2_Шаблоны классов/dec_based_on_two_vectors/deque_via_two_vectors.cpp
+++ 3_course/week 1/2_Шаблоны классов/dec_based_on_two_vectors/deque_via_two_vectors.cpp
@@ -1,9 +1,6 @@
 //#include "test_runner.h"
-//#include <utility>
-//#include <iostream>
 #include <stdexcept>
 #include <vector>
-//#include <deque>
 
 using namespace std;
 
@@ -13,11 +10,9 @@ class Deque {
 public:
 
     Deque() {
-        //front_;
-        //back_;
     }
 
-    Deque(vector<T> front) : front_(front) {
+    Deque(vector<T> front) : front_(front.rbegin(), front.rend()) {
     }
 
     bool Empty() const {
@@ -29,179 +24,146 @@ public:
     }
 
     T& operator[](size_t index) {
-        if (front_.size() > index) {
-            return front_[index];
-        }
-        else {
-            return back_[(Size() - 1) - index];
-        }
+        return (front_.size() > index) ? front_[(front_.size() - 1) - index] : back_[index - front_.size()];
     }
 
     const T& operator[](size_t index) const {
-        if (front_.size() > index) {
-            return front_[index];
-        }
-        else {
-            return back_[(Size() - 1) - index];
-        }
+        return (front_.size() > index) ? front_[(front_.size() - 1) - index] : back_[index - front_.size()];
     }
 
     T& At(size_t index) {
-        if (index >= Size()) {
-            throw out_of_range("invalid deque<T> subscript");
-        }
-        else if (front_.size() > index) {
-            return front_[index];
-        }
-        else {
-            return back_[(Size() - 1) - index];
-        }
+        CheckExcept(index);
+        return (front_.size() > index) ? front_[(front_.size() - 1) - index] : back_[index - front_.size()];
     }
 
     const T& At(size_t index) const {
-        if (index >= Size()) {
-            throw out_of_range("invalid deque<T> subscript");
-        }
-        else if (front_.size() > index) {
-            return front_[index];
-        }
-        else {
-            return back_[(Size() - 1) - index];
-        }
+        CheckExcept(index);
+        return (front_.size() > index) ? front_[(front_.size() - 1) - index] : back_[index - front_.size()];
     }
 
     T& Front() {
-        return front_.front();
+        return front_.back();
     }
 
     const T& Front() const {
-        return front_.front();
+        return front_.back();
     }
 
     T& Back() {
-        if (back_.empty()) {
-            return front_.back();
-        }
-        else {
-            return back_.back();
-        }
+        return back_.empty() ? front_.front() : back_.back();
     }
 
     const T& Back() const {
-        if (back_.empty()) {
-            return front_.back();
-        }
-        else {
-            return back_.back();
-        }
+        return back_.empty() ? front_.front() : back_.back();
     }
 
     void PushFront(T indx) {
-        front_.insert(front_.begin(), indx);
+        front_.push_back(indx);
     }
 
     void PushBack(T indx) {
-        if (front_.empty()) {
-            front_.push_back(indx);
-        }
-        else {
-            back_.push_back(indx);
-        }
+        return front_.empty() ? front_.push_back(indx) : back_.push_back(indx);
     }
 
 private:
+    void CheckExcept(size_t index) const {
+        if (index >= Size()) {
+            throw out_of_range("invalid deque<T> subscript");
+        }
+    }
+
     vector<T> front_, back_;
 };
 /*
-void TestDeque() {
+void TestEmpty() {
+    Deque<int> d;
+    ASSERT(d.Empty());
+}
+
+void TestSize() {
+    Deque<int> d({ 1,2,3,4,5 });
+    ASSERT_EQUAL(d.Size(), 5u);
 
     Deque<int> d1;
-    ASSERT_EQUAL(d1.Empty(), true);
+    ASSERT_EQUAL(d1.Size(), 0u);
+}
 
-    Deque<int> d2({ 1,2,3,4,5 });
-    ASSERT_EQUAL(d2.Size(), 5u);
-    d2[0] = 42;
-    ASSERT_EQUAL(d2.Size(), 5u);
-    d2.At(0) = 100;
-    ASSERT_EQUAL(d2.Front(), 100);
-    d2.PushFront(112);
-    ASSERT_EQUAL(d2.Front(), 112);
-    d2.PushBack(12);
-    ASSERT_EQUAL(d2.Back(), 12);
-    ASSERT_EQUAL(d2.Size(), 7);
-    d2.At(6) = 1000;
-    ASSERT_EQUAL(d2.Back(), 1000);
-    d2[0] = 1111;
-    ASSERT_EQUAL(d2.Front(), 1111);
-    d2[6] = 2222;
-    ASSERT_EQUAL(d2.Back(), 2222);
-    d2[5] = 55;
-    d2.At(5) = 555;
-    ASSERT_EQUAL(d2.Size(), 7);
-    d2.At(0) = 1;
-    d2.At(1) = 2;
-    d2.At(2) = 3;
-    d2.At(3) = 4;
-    d2.At(4) = 5;
-    d2.At(5) = 6;
-    d2.At(6) = 7;
-    d2[0] = 11;
-    d2[1] = 22;
-    d2[2] = 33;
-    d2[3] = 44;
-    d2[4] = 55;
-    d2[5] = 66;
-    d2[6] = 77;
+void TestOperator() {
+    Deque<string> d({ "A","B","C","D" });
+    d[0] = "E";
+    ASSERT_EQUAL(d[0], "E");
+    ASSERT_EQUAL(d[1], "B");
+    ASSERT_EQUAL(d[2], "C");
+    ASSERT_EQUAL(d[3], "D");
 
-    Deque<string> d3;
-    d3.PushBack("A");
-    cout << "Size = " << d3.Size() << endl;
-    cout << d3.Front() << "-" << d3.Back() << endl;
+    d.PushFront("0");
+    d.PushBack("E");
+    ASSERT_EQUAL(d[0], "0");
+    ASSERT_EQUAL(d[5], "E");
+    d.PushFront("1");
+    d.PushBack("F");
+    ASSERT_EQUAL(d[0], "1");
+    ASSERT_EQUAL(d[7], "F");
+}
 
-    Deque<string> d4;
-    d4.PushFront("B");
-    cout << "Size = " << d4.Size() << endl;
-    cout << d4.Front() << "-" << d4.Back() << endl;
+void TestAt() {
+    TestOperator();
 
-    deque<string> d5;
-    d5.push_back("a");
-    cout << "Size = " << d5.size() << endl;
-    cout << d5.front() << "-" << d5.back() << endl;
+    Deque<string> d({ "A","B","C","D" });
+    //d.At(10) = "E";
+}
 
-    deque<string> d6;
-    d6.push_front("a");
-    cout << "Size = " << d6.size() << endl;
-    cout << d6.front() << "-" << d6.front() << endl;
+void TestFrontAndBack() {
+    Deque<string> d({ "A","B","C","D" });
 
-    deque<int> d7({ 1,2,3,4,5 });
-    d7[0] = 42;
-    d7.at(0) = 100;
-    d7.push_front(112);
-    d7.push_back(12);
-    d7.at(6) = 1000;
-    d7[0] = 1111;
-    d7[6] = 2222;
-    d7[5] = 55;
-    d7.at(5) = 555;
-    d7.at(0) = 1;
-    d7.at(1) = 2;
-    d7.at(2) = 3;
-    d7.at(3) = 4;
-    d7.at(4) = 5;
-    d7.at(5) = 6;
-    d7.at(6) = 7;
-    d7[0] = 11;
-    d7[1] = 22;
-    d7[2] = 33;
-    d7[3] = 44;
-    d7[4] = 55;
-    d7[5] = 66;
-    d7[6] = 77;
+    d.PushFront("0");
+    d.PushBack("E");
+    ASSERT_EQUAL(d[0], d.Front());
+    ASSERT_EQUAL(d[d.Size()-1], d.Back());
+
+    d.PushFront("1");
+    d.PushFront("2");
+    d.PushBack("F");
+    d.PushBack("G");
+    ASSERT_EQUAL(d[0], d.Front());
+    ASSERT_EQUAL(d[d.Size()-1], d.Back());
+
+    Deque<string> d1;
+    d1.PushFront("A");
+    ASSERT_EQUAL(d1.Front(), "A");
+    ASSERT_EQUAL(d1.Back(), "A");
+
+    Deque<string> d2({"A"});
+    ASSERT_EQUAL(d2.Front(), "A");
+    ASSERT_EQUAL(d2.Back(), "A");
+}
+
+void TestPushFrontAndBack() {       // CBADE
+    Deque<string> d({ "A" });
+    d.PushFront("B");
+    d.PushFront("C");
+    ASSERT_EQUAL(d.Front(), "C");
+    ASSERT_EQUAL(d.Back(), "A");
+    d.PushBack("D");
+    d.PushBack("E");
+    ASSERT_EQUAL(d.Front(), "C");
+    ASSERT_EQUAL(d.Back(), "E");
+    ASSERT_EQUAL(d[0], "C");
+    ASSERT_EQUAL(d[1], "B");
+    ASSERT_EQUAL(d[2], "A");
+    ASSERT_EQUAL(d[3], "D");
+    ASSERT_EQUAL(d[4], "E");
+    ASSERT_EQUAL(d.Back(), "E");
 }
 
 int main() {
     TestRunner tr;
-    RUN_TEST(tr, TestDeque);
+    RUN_TEST(tr, TestEmpty);
+    RUN_TEST(tr, TestSize);
+    RUN_TEST(tr, TestOperator);
+    RUN_TEST(tr, TestAt);
+    RUN_TEST(tr, TestFrontAndBack);
+    RUN_TEST(tr, TestPushFrontAndBack);
     return 0;
 }
 */
