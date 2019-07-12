78d982f9d0ca404dc2953535ccb6725dbdc89577
UltraLordEdition <maxplank02@gmail.com>
2019-07-12 16:47:08 +0300

    final

--- 3_course/week 5/2_Базовая многопоточность/ConcurrentMap/concurrent_map.cpp
+++ 3_course/week 5/2_Базовая многопоточность/ConcurrentMap/concurrent_map.cpp
@@ -2,89 +2,92 @@
 #include "profile.h"
 
 #include <algorithm>
-#include <utility>
 #include <numeric>
 #include <vector>
 #include <string>
 #include <random>
-#include <future>
 #include <mutex>
-#include <map>
-
+#include <future>
+#include <cstdlib>
 using namespace std;
 
 template <typename K, typename V>
-class ConcurrentMap {
+class ConcurrentMap
+{
 public:
     static_assert(is_integral_v<K>, "ConcurrentMap supports only integer keys");
 
-    struct Access {
-        V& ref_to_value;
-        lock_guard<mutex> guard;
+    struct Access
+    {
+        lock_guard<mutex> g;
+        V &ref_to_value;
     };
 
-    explicit ConcurrentMap(size_t bucket_count) : thread_count_(bucket_count),  base_(bucket_count) {
+    explicit ConcurrentMap(size_t bucket_count) : _bucketCount(bucket_count), _maps(bucket_count)
+    {
     }
 
-    Access operator[](const K& key) {
-        return { base_[getCounter() - 1u][key], lock_guard(m) };
+    Access operator[](const K &key)
+    {
+        return {lock_guard(_maps[keyToIndex(key)].m), _maps[keyToIndex(key)].data[key]};
     }
-   
-    map<K, V> BuildOrdinaryMap() {
-        LOG_DURATION("BuildOrdinaryMap()");
-        for (auto& it : base_) {
-            for (const auto&[key, value] : it) {
-                if (submap_.count(key)) {
-                    submap_[key] += value;
-                }  
-                submap_.insert({ key,value });
-            }
+
+    map<K, V> BuildOrdinaryMap()
+    {
+        map<K, V> result;
+
+        for (auto &i : _maps)
+        {
+            lock_guard<mutex> g(i.m);
+            result.insert(i.data.begin(), i.data.end());
         }
-        return move(submap_);
-    }
 
+        return result;
+    }
 private:
-    size_t& getCounter() {
-        lock_guard<mutex> g(m);
-        if (counter_ < thread_count_) {
-            counter_++;
-        }
-        else {
-            counter_ = 1;
-        }
-        return counter_;
+    struct mutexMap
+    {
+        map<K, V> data;
+        mutex m;
+    };
+
+    const size_t _bucketCount;
+    vector<mutexMap> _maps;
+
+    size_t keyToIndex(const K &k)
+    {
+        return abs(static_cast<long int>(k)) % _bucketCount;
     }
-    
-    const size_t thread_count_;
-    size_t counter_;
-    map<K, V> submap_;
-    vector<map<K, V>> base_;
-    mutex m;
 };
 
-void RunConcurrentUpdates(ConcurrentMap<int, int>& cm, size_t thread_count, int key_count) {
-    LOG_DURATION("kernel");
-    auto kernel = [&cm, key_count](int seed) {
+void RunConcurrentUpdates(
+    ConcurrentMap<int, int> &cm, size_t thread_count, int key_count
+)
+{
+    auto kernel = [&cm, key_count](int seed)
+    {
         vector<int> updates(key_count);
         iota(begin(updates), end(updates), -key_count / 2);
         shuffle(begin(updates), end(updates), default_random_engine(seed));
 
-        for (int i = 0; i < 2; ++i) {
-            for (auto key : updates) {
+        for (int i = 0; i < 2; ++i)
+        {
+            for (auto key : updates)
+            {
                 cm[key].ref_to_value++;
             }
         }
     };
-    
-    {LOG_DURATION("vector_future");
+
     vector<future<void>> futures;
-    for (size_t i = 0; i < thread_count; ++i) {
+    for (size_t i = 0; i < thread_count; ++i)
+    {
         futures.push_back(async(kernel, i));
-    }
     }
 }
 
-void TestConcurrentUpdate() {
+void TestConcurrentUpdate()
+{
     const size_t thread_count = 3;
     const size_t key_count = 50000;
 
@@ -93,22 +96,28 @@ void TestConcurrentUpdate() {
 
     const auto result = cm.BuildOrdinaryMap();
     ASSERT_EQUAL(result.size(), key_count);
-    for (auto&[k, v] : result) {
+    for (auto& [k, v] : result)
+    {
         AssertEqual(v, 6, "Key = " + to_string(k));
     }
 }
 
-void TestReadAndWrite() {
+void TestReadAndWrite()
+{
     ConcurrentMap<size_t, string> cm(5);
 
-    auto updater = [&cm] {
-        for (size_t i = 0; i < 50000; ++i) {
+    auto updater = [&cm]
+    {
+        for (size_t i = 0; i < 50000; ++i)
+        {
             cm[i].ref_to_value += 'a';
         }
     };
-    auto reader = [&cm] {
+    auto reader = [&cm]
+    {
         vector<string> result(50000);
-        for (size_t i = 0; i < result.size(); ++i) {
+        for (size_t i = 0; i < result.size(); ++i)
+        {
             result[i] = cm[i].ref_to_value;
         }
         return result;
@@ -122,15 +131,18 @@ void TestReadAndWrite() {
     u1.get();
     u2.get();
 
-    for (auto f : { &r1, &r2 }) {
+    for (auto f : {&r1, &r2})
+    {
         auto result = f->get();
-        ASSERT(all_of(result.begin(), result.end(), [](const string& s) {
+        ASSERT(all_of(result.begin(), result.end(), [](const string & s)
+        {
             return s.empty() || s == "a" || s == "aa";
         }));
     }
 }
 
-void TestSpeedup() {
+void TestSpeedup()
+{
     {
         ConcurrentMap<int, int> single_lock(1);
 
@@ -142,17 +154,11 @@ void TestSpeedup() {
 
         LOG_DURATION("100 locks");
         RunConcurrentUpdates(many_locks, 4, 50000);
-
-        /*
-        const auto result = many_locks.BuildOrdinaryMap();
-        ASSERT_EQUAL(result.size(), 50000);
-        for (auto&[k, v] : result) {
-            AssertEqual(v, 8, "Key = " + to_string(k));
-        }*/
     }
 }
 
-int main() {
+int main()
+{
     TestRunner tr;
     RUN_TEST(tr, TestConcurrentUpdate);
     RUN_TEST(tr, TestReadAndWrite);
